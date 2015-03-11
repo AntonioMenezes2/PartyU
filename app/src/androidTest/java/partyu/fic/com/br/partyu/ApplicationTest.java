5731ed64cbbedb5512ec78b2131cc8e3c3f1e23d
AntonioMenezes2 <netynho_antonio@hotmaial.com>
2015-03-10 21:31:53 -0300

    Update ApplicationTest.java

--- app/src/androidTest/java/partyu/fic/com/br/partyu/ApplicationTest.java
+++ app/src/androidTest/java/partyu/fic/com/br/partyu/ApplicationTest.java
@@ -1,9 +1,10 @@
 package partyu.fic.com.br.partyu;
 
 import android.app.Application;
-import android.test.ApplicationTestCase;
+impsdfsffsfsdfsdort android.test.ApplicationTestCase;
 
 /**
+teste
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
